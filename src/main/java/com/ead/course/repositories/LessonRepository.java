e5af04750648198cf07c5485567786fd6f3be353
Reginaldo Andrade <reginaldo.andrade@outlook.com>
2022-07-23 21:38:00 -0300

    fix: lesson repository

--- src/main/java/com/ead/course/repositories/LessonRepository.java
+++ src/main/java/com/ead/course/repositories/LessonRepository.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LessonRepository extends JpaRepository<LessonRepository, UUID> {
+import com.ead.course.models.LessonModel;
 
+public interface LessonRepository extends JpaRepository<LessonModel, UUID> {
 }
