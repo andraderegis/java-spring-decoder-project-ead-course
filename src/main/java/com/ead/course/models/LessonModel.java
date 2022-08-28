c50ba14b03754fe87698ddbd8865ef99e543f8fa
Reginaldo Andrade <reginaldo.andrade@outlook.com>
2022-08-27 23:03:02 -0300

    chore: remove unused imports

--- src/main/java/com/ead/course/models/LessonModel.java
+++ src/main/java/com/ead/course/models/LessonModel.java
@@ -13,8 +13,6 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-import org.hibernate.annotations.Fetch;
-import org.hibernate.annotations.FetchMode;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
